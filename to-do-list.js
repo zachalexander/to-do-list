c05dcff8050a25316fefc91dab854875db5aacd5
John Coppola <jcoppola@instien.com>
2018-05-29 19:40:01 -0400

    updates add function expands dom

--- to-do-list.js
+++ to-do-list.js
@@ -1,24 +1,38 @@
-
-
 let taskArray = [];
+const $input = document.getElementById('input');
+const $button = document.querySelector('.add-list-item');
+const $taskList = document.querySelector('.task-list');
 
-// function addTask(str) {
-//   taskArray.push(str);
-//   return taskArray;
-// }
+$button.addEventListener('click', whenButtonClick);
+
 
 function whenButtonClick() {
-  let input = document.getElementById('input').value;
-  taskArray.push(input);
-  console.log(taskArray);
-  // let output = addTask(getInput);
-  // console.log(output);
-  // return output;
+
+	taskArray.push($input.value);
+	console.log('from inside when button click', taskArray);
+
+	let $listItem = document.createElement("li");
+	let $listText = document.createElement("p");
+	let $deleteButton = document.createElement("button");
+
+	$listText.innerText = $input.value;
+	$deleteButton.innerText = "Delete";
+	$taskList.append($listItem);
+	$listItem.append($listText);
+	$listItem.append($deleteButton);
+
+	$deleteButton.addEventListener('click', deleteItem);
+
 }
 
+function deleteItem(event) {
+	console.log(event.target);
+}
 // let input = whenButtonClick();
 // console.log(input);
 
 // document.getElementById("myList").innerHTML = whenButtonClick();
 
 //add event listener (pass the click and callback function)
+
+//event.target browser gives a way to interact with DOM and point to specific thing
