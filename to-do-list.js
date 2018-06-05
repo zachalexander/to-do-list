e7f8ca877800a39d9a66cd8e0ce8987c26520ebf
zachalexander <zacharya@cep.org>
2018-06-05 19:47:38 -0400

    adds progress on edit feature

--- to-do-list.js
+++ to-do-list.js
@@ -17,9 +17,11 @@ function whenButtonClick() {
 	let $editButton = document.createElement("button");
 
 	$listText.innerText = $input.value;
+  $listText.classList.add("task-list__item-name");
 	$deleteButton.innerText = "Delete";
 	$editButton.innerText = "Edit";
 	$taskList.append($listItem);
+  $listItem.classList.add("task-list__item");
 	$listItem.append($listText);
 	$listItem.append($deleteButton);
 	$listItem.append($editButton);
@@ -32,32 +34,49 @@ function whenButtonClick() {
 
 function deleteItem(event) {
   // finding the listItem HTML <li> tag.
-  const $selectedTaskToDeleteHtml = event.path[1];
+  const $selectedTask = event.path[1];
+  console.log(event);
   // accessing the text of the listItem. firstElementChild = <p></p>
-  const $selectedTaskToDelete = event.path[1].firstElementChild.innerHTML;
+  const selectedTaskName = event.path[1].firstElementChild.innerHTML;
   // removing the listItem from the DOM. parentNode = <li></li>
-  $selectedTaskToDeleteHtml.parentNode.removeChild($selectedTaskToDeleteHtml);
+  $selectedTask.parentNode.removeChild($selectedTask);
   // finding the index of $selectedTaskToDelete in taskArray
-  let index = taskArray.indexOf($selectedTaskToDelete);
+  let index = taskArray.indexOf(selectedTaskName);
   // splicing this string out of taskArray by it's index. Second parameter states how many to remove.
   taskArray.splice(index, 1);
-  console.log(taskArray);
 }
 
 function editItem(event) {
-	const $selectedTaskToEditHTML = event.path[1];
-	const $selectedTaskToEdit = event.path[1].firstElementChild.innerHTML;
-	console.log($selectedTaskToEdit);
+	const $selectedTask = event.path[1];
+  const $selectedTaskText = $selectedTask.firstElementChild;
+	let selectedTaskName = $selectedTaskText.innerText;
 
-	$selectedTaskToEditHTML.parentNode.removeChild($selectedTaskToEditHTML);
+	// $selectedTaskToEditHTML.parentNode.removeChild($selectedTaskToEditHTML);
+  $selectedTask.classList.add("hide");
 
 	let $editListItem = document.createElement("li");
-	let $editListInput = document.createElement("INPUT");
+  $editListItem.classList.add("task-list__edit-item");
+	let $editListInput = document.createElement("input");
+  let $editListButton = document.createElement("button");
 	// let $editListText = document.createElement("p");
 
 	$taskList.append($editListItem);
 	$editListItem.append($editListInput);
-	
+  $editListItem.append($editListButton);
+  $editListButton.innerText = "Edit";
+
+  $editListInput.value = selectedTaskName;
+
+  $editListButton.addEventListener('click', function() {
+    const updatedTaskName = $editListInput.value;
+    console.log($selectedTask);
+    $selectedTaskText.innerText = updatedTaskName;
+    $selectedTask.classList.remove("hide");
+    $editListItem.parentNode.removeChild('task-list__edit-item');
+    // selectedTaskName = updatedTaskName;
+
+  });
+
 	// $editListItem.append($editListText);
 
 
