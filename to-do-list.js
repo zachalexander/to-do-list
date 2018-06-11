5be25b157eb8e9f39ec91bdbb97ee751ffc30118
John Coppola <jcoppola@instien.com>
2018-06-11 19:46:02 -0400

    organizes code structure by function fixes bugs in edit

--- to-do-list.js
+++ to-do-list.js
@@ -1,85 +1,102 @@
 let taskArray = [];
 const $input = document.querySelector('input');
-const $button = document.querySelector('.add-list-item');
+const $addButton = document.querySelector('.add-list-item');
 const $taskList = document.querySelector('.task-list');
 
-$button.addEventListener('click', whenButtonClick);
+$addButton.addEventListener('click', addItem);
 
-
-function whenButtonClick() {
-
-	taskArray.push($input.value);
-
+function addItem() {
+	// UI
+	// create elements
 	let $listItem = document.createElement("li");
 	let $listText = document.createElement("p");
 	let $deleteButton = document.createElement("button");
 	let $editButton = document.createElement("button");
-
-	$listText.innerText = $input.value;
+	
+	// add classes
+	$listItem.classList.add("task-list__item");
   $listText.classList.add("task-list__item-name");
-	$deleteButton.innerText = "Delete";
 	$deleteButton.classList.add("task-list__delete-button");
-	$editButton.innerText = "Edit";
 	$editButton.classList.add("task-list__edit-button");
+	
+	// add innerText to elements
+	$listText.innerText = $input.value;
+	$deleteButton.innerText = "Delete";
+	$editButton.innerText = "Edit";
+	
+	// adding EventListeners
+	$deleteButton.addEventListener('click', deleteItem);
+	$editButton.addEventListener('click', editItem);
+	
+	// appending elements to DOM
 	$taskList.append($listItem);
-  $listItem.classList.add("task-list__item");
 	$listItem.append($listText);
 	$listItem.append($deleteButton);
 	$listItem.append($editButton);
-
-	$deleteButton.addEventListener('click', deleteItem);
-	$editButton.addEventListener('click', editItem);
+	
+	// STATE
+	taskArray.push($input.value);
+	console.log(taskArray);
 
 }
 
-
 function deleteItem(event) {
+	// UI
   const $selectedTask = event.target;
-  const selectedTaskParent = $selectedTask.parentNode;
-	const taskToDelete = selectedTaskParent.querySelector('.task-list__item-name').innerText;
-  $selectedTask.parentNode.remove('task-list__item');
-  let index = taskArray.indexOf(taskToDelete);
-  taskArray.splice(index, 1);
+  const $listItem = $selectedTask.parentNode;
+	const taskToDelete = $listItem.querySelector('.task-list__item-name').innerText;
+	$selectedTask.parentNode.remove('task-list__item');
+	
+	// STATE
+	let index = taskArray.indexOf(taskToDelete);
+	taskArray.splice(index, 1);
+	console.log(taskArray);
 }
 
 function editItem(event) {
+
+	// UI
+	// hide element we are editing
 	const $selectedTask = event.target;
-	const selectedTaskParent = $selectedTask.parentNode;
-	let selectedTaskName = $selectedTask.innerText;
-
-
-
-	// $selectedTaskToEditHTML.parentNode.removeChild($selectedTaskToEditHTML);
-  selectedTaskParent.classList.add("hide");
-
-
-// start from here...
+	const $listItem = $selectedTask.parentNode;
+	$listItem.classList.add("hide");
+	
+	// create elements
 	let $editListItem = document.createElement("li");
-  $editListItem.classList.add("task-list__edit-item");
 	let $editListInput = document.createElement("input");
-  let $editListButton = document.createElement("button");
-	// let $editListText = document.createElement("p");
+	let $editListButton = document.createElement("button");
 
+	// add class names
+	$editListItem.classList.add("task-list__edit-item");
+	$editListInput.classList.add("task-list__edit-input");
+	$editListButton.classList.add("task-list__edit-button");
+	
+	// add innerText to elements
+	const $taskToEdit = $listItem.querySelector('.task-list__item-name');
+	const task = $taskToEdit.innerText;
+	$editListInput.value = task;
+
+	$editListButton.innerText = "Save";
+	
+	// add EventListeners
+	$editListButton.addEventListener('click', function(){
+		// update and show the element that we are editing
+		const inputValue = $editListInput.value;
+		$taskToEdit.innerText = inputValue;
+		$listItem.classList.remove("hide");
+
+		// remove edit box from UI
+		const test = $editListItem.parentElement.removeChild($editListItem);
+
+		// STATE
+		let index = taskArray.indexOf(task);
+		taskArray[index] = inputValue;
+		console.log(taskArray);
+	});
+
+	
+	// appending elements to DOM
 	$taskList.append($editListItem);
 	$editListItem.append($editListInput);
   $editListItem.append($editListButton);
-  $editListButton.innerText = "Edit";
-
-  $editListInput.value = selectedTaskName;
-
-  $editListButton.addEventListener('click', function() {
-    const updatedTaskName = $editListInput.value;
-    console.log($selectedTask);
-    selectedTaskName.innerText = updatedTaskName;
-    selectedTaskParent.classList.remove("hide");
-    $editListItem.parentNode.removeChild('task-list__edit-item');
-    // selectedTaskName = updatedTaskName;
-
-  });
-
-	// $editListItem.append($editListText);
-
-
-
-
 }
